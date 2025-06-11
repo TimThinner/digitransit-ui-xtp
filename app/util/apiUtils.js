8c6c3682212a32a467a35838a4d21a834b06b821
Timo Kinnunen <timo.kinnunen@vtt.fi>
2025-06-11 12:19:32 +0300

    Added mock data for XTP info testing.

--- app/util/apiUtils.js
+++ app/util/apiUtils.js
@@ -64,7 +64,7 @@ export function getWeatherData(baseURL, time, lat, lon) {
       throw new Error(`Error fetching weather data: ${err}`);
     });
 }
-
+/*
 export function getXTPInfoList(baseURL, data) {
   const options = {
     method: 'POST',
@@ -74,6 +74,86 @@ export function getXTPInfoList(baseURL, data) {
     body: JSON.stringify(data),
   };
   return retryFetch(baseURL, 0, 0, options).then(res => res.json());
+}*/
+export function getXTPInfoList(baseURL, data) {
+/*
+data:{
+  edges:[
+    edge_index:0, 
+    legs:[
+      leg_index:0,
+      from:{lat: ,lon: ,name:"huuhaa"},
+      to:{lat: ,lon: ,name:"huuhaa"},
+      legGeometry:{...}
+    ],
+    ...
+  ]
+}
+*/
+	const mock_data = {infos:[]};
+	if (data && data.edges && Array.isArray(data.edges) && data.edges.length > 0) {
+		data.edges.forEach(e=>{
+			const ei = e.edge_index;
+			if (e.legs && Array.isArray(e.legs) && e.legs.length > 0) {
+				e.legs.forEach(leg=>{
+					mock_data.infos.push({
+						edge_index: ei,
+						leg_index: leg.leg_index,
+						type: 'photo',
+						url: 'https://picsum.photos/200',
+						lat:  leg.from.lat,
+						lon: leg.from.lon,
+						name: leg.from.name
+					});
+				});
+			}
+		});
+	}
+	return this.retryFetch(
+		'https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow',
+		2,
+		200
+	)
+	.then(res => res.json())
+	.then(json => {
+		//console.log(['json=',json]);
+		console.log(['mock_data=',mock_data]);
+		return mock_data;
+		//return json;
+	})
+	.catch(err => {
+		throw new Error(`Error fetching XTP Info List: ${err}`);
+	});
+  /*
+  return new Promise(function(resolve) {
+    const promises = [];
+    const mock_data = [
+      {
+        title:"Nauvo"
+      }
+    ];
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+    const p = retryFetch(
+      'https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow',
+      options,
+      2,
+      200);
+    promises.push(p);
+    const nested=[];
+    Promise.all(promises).then(res => {
+      res.forEach(r=>{
+        nested.push(r.json());
+      });
+      Promise.all(nested).then(data=>{
+        resolve(mock_data);
+      });
+    });
+  });*/
 }
 
 export function getRefPoint(origin, destination, location) {
