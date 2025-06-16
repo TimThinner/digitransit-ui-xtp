0b3a48749fc330145c827a2082e7afa8ce3c0f96
Timo Kinnunen <timo.kinnunen@vtt.fi>
2025-06-16 15:07:30 +0300

    Test now without handleClick. See if compile OK.

--- app/component/map/popups/XtpPopup.js
+++ app/component/map/popups/XtpPopup.js
@@ -7,11 +7,6 @@ import { isBrowser } from '../../../util/browser';
 const Popup = isBrowser ? require('react-leaflet/es/Popup').default : null; // eslint-disable-line global-require
 
 export default function XtpPopup({ lat, lon, xtpurl }) {
-  
-  function handleClick() {
-    console.log('You clicked me!');
-  }
-  
   return (
     <Popup
       position={{ lat: lat+0.0001, lng: lon }}
@@ -25,7 +20,7 @@ export default function XtpPopup({ lat, lon, xtpurl }) {
       <Card className="no-margin">
         <div className="location-popup-wrapper">
           <div className="location-thumbnail-image">
-            <img onClick={handleClick} src={xtpurl} width="300" height="400" />
+            <img src={xtpurl} width="300" height="400" />
           </div>
         </div>
       </Card>
