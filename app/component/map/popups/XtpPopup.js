99c61e40fe0519ba1e1f7710237f7189e6b6d89a
Timo Kinnunen <timo.kinnunen@vtt.fi>
2025-06-17 14:30:17 +0300

    Test zooming in XTP Popup PART 4.

--- app/component/map/popups/XtpPopup.js
+++ app/component/map/popups/XtpPopup.js
@@ -9,32 +9,32 @@ import useWindowSize from '../../../hooks/useWindowSize';
 const Popup = isBrowser ? require('react-leaflet/es/Popup').default : null; // eslint-disable-line global-require
 
 export default function XtpPopup({ lat, lon, xtpurl }) {
-  const [imgSize, setImgSize] = useState({fullscreen:false, width:150, height:200});
-  const size = useWindowSize();
-
-  console.log(['XtpPopup size=',size]);
-
+  const [imgSize, setImgSize] = useState({fullscreen:false, width:300, height:400});
+  
+  console.log(['imgSize.width=',imgSize.width, 'imgSize.height=',imgSize.height]);
+  
   function handleClick() {
     console.log('You clicked image!');
     if (imgSize.fullscreen) {
       // back to small size
-      setImgSize({fullscreen:false, width:150, height:200});
+      setImgSize({fullscreen:false, width:300, height:400});
     } else {
       // make image fullscreen
-      setImgSize({fullscreen:true, width:size.width-40, height:size.height-40});
+      const size = useWindowSize();
+      // Keep aspect ratio 3/4
+      const new_w = 300*size.height/400;
+      console.log(['XtpPopup size=',size, 'new_w=',new_w]);
+      setImgSize({fullscreen:true, width:new_w, height:size.height});
     }
   }
-  
-  // size.width,
-  // size.outer.width
   
   return (
     <Popup
       position={{ lat: lat+0.0001, lng: lon }}
       offset={[0, 0]}
       autoPanPaddingTopLeft={[5, 125]}
-      maxWidth={size.width}
-      maxHeight={size.height}
+      maxWidth={imgSize.width}
+      maxHeight={imgSize.height}
       autoPan={false}
       className="popup single-popup"
     >
