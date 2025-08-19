1f54246249d301bde2db5aef1cbb13e31ad3679f
Timo Kinnunen <timo.kinnunen@vtt.fi>
2025-08-19 16:13:47 +0300

    DEBUGGING PART 2.

--- app/component/map/LocationMarker.js
+++ app/component/map/LocationMarker.js
@@ -83,16 +83,6 @@ export default function LocationMarker({
       }}
       zIndexOffset={12000}
     >
-      {validType === 'xtp' && (
-        <XtpTestPopup
-          lat={xtp.lat}
-          lon={xtp.lon}
-          xtpurl={xtp.url}
-          key={`${xtp.lat}${xtp.lon}`}
-          xtp_last_index={xtp_last_index}
-          pid={pid}
-        />
-      )}
       {validType === 'via' && (
         <ViaPointPopup
           lat={position.lat}
