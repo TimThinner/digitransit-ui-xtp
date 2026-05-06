956fc6fe38f6c3bb4dc8dcbef8a052b31b3ab31e
Timo Kinnunen <timo.kinnunen@vtt.fi>
2026-05-06 14:29:44 +0300

    Try to fix unit-test error PART 1.

--- app/component/itinerary/Itinerary.js
+++ app/component/itinerary/Itinerary.js
@@ -908,7 +908,7 @@ const Itinerary = ({
     !containsScooterLeg;
   
   /* Show the "camera" icon if this edge is listed in xtpPoints. */
-  const showXTPInfoIcon = xtpPoints.find((item) => item.edge_index === props.hash);
+  const showXTPInfoIcon = xtpPoints ? xtpPoints.find((item) => item.edge_index === props.hash) : null;
   
   const itineraryContainerOverflowRef = createRef();
   const [showOverflowIcon, setShowOverflowIcon] = useState(false);
